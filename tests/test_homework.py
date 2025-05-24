24b8a842e6b93fde8392b6494ba3d03d995a0695
ycrossa <116770244+ycrossa@users.noreply.github.com>
2025-05-24 11:28:51 -0500

    PRE-08_mlflow_tracking_basico

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -11,26 +11,21 @@ def test_01():
 
     # Test if the homework script runs without errors
     try:
-        for entry_point in ["elasticnet", "knn"]:
+        for model in ["elasticnet", "knn"]:
             subprocess.run(
-                [
-                    "mlflow",
-                    "run",
-                    "--env-manager=local",
-                    "-e",
-                    entry_point,
-                    ".",
-                ],
+                ["python", "-m", "homework", "--model", model],
                 check=True,
             )
     except subprocess.CalledProcessError as e:
         raise Exception(f"Error running the homework script: {e}")
 
     # Ensure the mlruns directory exists
-    assert os.path.exists("mlruns"), "mlruns directory does not exist."
+    assert os.path.exists("my_mlruns"), "mlruns directory does not exist."
 
     # Check if there are any experiments saved in mlruns/
     experiments = [
-        d for d in os.listdir("mlruns") if os.path.isdir(os.path.join("mlruns", d))
+        d
+        for d in os.listdir("my_mlruns")
+        if os.path.isdir(os.path.join("my_mlruns", d))
     ]
     assert len(experiments) > 0, "No experiments found in mlruns directory."
